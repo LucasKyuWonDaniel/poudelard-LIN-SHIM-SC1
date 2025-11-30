6b9ad9ed90968e826cfacf852f62cc2d7e060abe
Daniel <kyu.shim@efrei.net>
2025-11-30 01:19:04 +0100

    Creation des fichiers

---
+++ Poudlard/utils/input_utils.py
@@ -0,0 +1,31 @@
+def demander_texte(phrase):
+    if phrase == "":
+        return "the phrase is empty"
+    reponse = str(input(phrase ))
+    while len(reponse) < 1:
+        reponse = str(input(phrase ))
+    return reponse
+
+#Pas Pafait comme dans la consigne, à revoir
+def demander_nombre(phrase, min_value = None, max_value = None):
+    reponse = int(input(phrase ))
+    if min_value is None:
+        return reponse
+    if max_value is None:
+        return reponse
+    else :
+        while reponse < min_value or reponse > max_value:
+            reponse = int(input(phrase))
+    return reponse
+
+def demander_choix(phrase, choices):
+    print(phrase)
+    print()
+    for i in range (len(choices)):
+        print(i +1, choices[i])
+        print()
+    reponse = int(input())
+    while reponse <= 0 or reponse >= len(choices):
+        reponse = int(input())
+    return reponse
+
