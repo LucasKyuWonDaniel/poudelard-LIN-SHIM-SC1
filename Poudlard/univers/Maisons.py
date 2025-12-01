400873c7238fde00cf91dc461bd420c4925aac31
LucasKyuWonDaniel <kyu.shim@efrei.net>
2025-12-01 16:44:04 +0100

    Update Maisons.py

--- Poudlard/univers/Maisons.py
+++ Poudlard/univers/Maisons.py
@@ -4,3 +4,12 @@ def actualiser_points_maison(maisons, nom_maison, points) :
     print (nom_maison, ":", maisons[nom_maison])
   else :
     return " la maison est introuvable"
+
+def afficher_maison_gagnante(maisons) :
+  max = maisons["Gryffondor"]
+  for valeur in maisons.values() :
+    if max < valeur :
+      max = valeur
+  for cle in maisons.keys() :
+    if maisons[cle] == max :
+      print(cle)
