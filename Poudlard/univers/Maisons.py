b415d4724d1aa464edde795ad23291efafd0ab7f
LucasKyuWonDaniel <kyu.shim@efrei.net>
2025-12-03 17:51:00 +0100

    Update Maisons.py

--- Poudlard/univers/Maisons.py
+++ Poudlard/univers/Maisons.py
@@ -13,3 +13,30 @@ def afficher_maison_gagnante(maisons) :
   for cle in maisons.keys() :
     if maisons[cle] == max :
       print(cle)
+
+def repartition_maison(joueur, questions) :
+  score_maisons = {
+    "Gryffondor": 0, 
+    "Serpentard": 0, 
+    "Poufsouffle": 0, 
+    "Serdaigle": 0 
+  }
+
+
+questions = [ 
+    ( 
+        "Tu vois un ami en danger. Que fais-tu ?", 
+        ["Je fonce l'aider", "Je réfléchis à un plan", "Je cherche de l’aide", "Je reste calme et j’observe"], 
+        ["Gryffondor", "Serpentard", "Poufsouffle", "Serdaigle"] 
+    ), 
+    ( 
+        "Quel trait te décrit le mieux ?", 
+        ["Courageux et loyal", "Rusé et ambitieux", "Patient et travailleur", "Intelligent et curieux"], 
+        ["Gryffondor", "Serpentard", "Poufsouffle", "Serdaigle"] 
+    ), 
+    ( 
+        "Face à un défi difficile, tu...", 
+        ["Fonces sans hésiter", "Cherches la meilleure stratégie", "Comptes sur tes amis", "Analyses le problème"], 
+        ["Gryffondor", "Serpentard", "Poufsouffle", "Serdaigle"] 
+    ) 
+]
