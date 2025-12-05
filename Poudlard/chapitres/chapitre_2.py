e1523f05ef37370faf96baefb454e10285a97eea
LucasKyuWonDaniel <kyu.shim@efrei.net>
2025-12-05 08:24:59 +0100

    Update chapitre_2.py

--- Poudlard/chapitres/chapitre_2.py
+++ Poudlard/chapitres/chapitre_2.py
@@ -4,12 +4,9 @@ def rencontrer_amis(joueur) :
     print("Vous montez à bord du Poudlard Express. Le train démarre lentement en direction du Nord... ")
     print("Un garçon roux entre dans votre compartiment, l’air amical.")
     print("— Salut ! Moi c’est Ron Weasley. Tu veux bien qu’on s’assoie ensemble ? ")
-    L_réponse1 = ["1. Bien sûr, assieds-toi !", "2. Désolé, je préfère voyager seul."]
     print("Que répondez-vous ?")
-    ch
-    réponse1 = int(input("Votre choix :"))
-    while réponse1 != 1 and réponse1 != 2 :
-        réponse1 = int(input("Entrez une valeur correcte."))
+    L_réponse1 = ["Bien sûr, assieds-toi !", "Désolé, je préfère voyager seul."]
+    demander_choix("Votre choix :", L_réponse1)
     if réponse1 == 1 :
         joueur["Attributs"]["loyauté"] += 1
         print("Ron sourit : — Génial ! Tu verras, Poudlard, c’est incroyable ! ")
@@ -20,15 +17,12 @@ def rencontrer_amis(joueur) :
     print("Une fille entre ensuite, portant déjà une pile de livres.")
     print("— Bonjour, je m’appelle Hermione Granger. Vous avez déjà lu ‘Histoire de la Magie’ ?")
     print("Que répondez-vous ?")
-    print("1. Oui, j’adore apprendre de nouvelles choses ! ")
-    print("2. Euh… non, je préfère les aventures aux bouquins. ")
-    réponse2 = int(input("Votre choix :"))
-    while réponse2 != 1 and réponse2 != 2 :
-        réponse2 = int(input("Entrez une valeur correcte."))
+    L_réponse2 = ["Oui, j’adore apprendre de nouvelles choses !", "Euh… non, je préfère les aventures aux bouquins."]
+    demander_choix("Votre choix :", L_réponse2)
     if réponse2 == 1 :
         joueur["Attributs"]["intelligence"] += 1
         print("Hermione : — Super! Dans Histoire de la Magie, quel événement majeur Bathilda Tourdesac identifie-t-elle comme la cause première de l’adoption du Code International du Secret Magique ?")
-        input("")
+        input("Votre réponse :")
     else :
         joueur["Attributs"]["courage"] += 1
         print("Hermione fronce les sourcils : — Il faudrait pourtant s’y mettre un jour ! ")
@@ -36,12 +30,8 @@ def rencontrer_amis(joueur) :
     print("Puis un garçon blond entre avec un air arrogant.")
     print("— Je suis Drago Malefoy. Mieux vaut bien choisir ses amis dès le départ, tu ne crois pas ?")
     print("Comment réagissez-vous ? ")
-    print("1. Je lui serre la main poliment.")
-    print("2. Je l’ignore complètement.")
-    print("3. Je lui réponds avec arrogance.")
-    réponse3 = int(input("Votre choix :"))
-    while réponse3 < 1 or réponse3 > 3 :
-        réponse3 = int(input("Entrez une valeur correcte."))
+    L_réponse3 = ["Je lui serre la main poliment.", "Je l’ignore complètement.", "Je lui réponds avec arrogance."]
+    demander_choix("Votre choix :", L_réponse3)
     if réponse3 == 1 :
         joueur["Attributs"]["ambition"] += 1
         print("Drago : — HaHaHa! ")
@@ -54,6 +44,7 @@ def rencontrer_amis(joueur) :
     print("Le train continue sa route. Le château de Poudlard se profile à l’horizon... ")
     print("Tes choix semblent déjà en dire long sur ta personnalité !")
     print("Tes attributs mis à jour :", joueur["Attributs"])
+
 
 
 
