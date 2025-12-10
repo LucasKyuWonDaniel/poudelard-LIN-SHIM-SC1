83d1297898fabbdc7974e9c9937613c55a19c06b
LucasKyuWonDaniel <kyu.shim@efrei.net>
2025-12-10 19:11:28 +0100

    Update chapitre_2.py

--- Poudlard/chapitres/chapitre_2.py
+++ Poudlard/chapitres/chapitre_2.py
@@ -6,9 +6,8 @@ def rencontrer_amis(joueur) :
     print("Vous montez à bord du Poudlard Express. Le train démarre lentement en direction du Nord... ")
     print("Un garçon roux entre dans votre compartiment, l’air amical.")
     print("— Salut ! Moi c’est Ron Weasley. Tu veux bien qu’on s’assoie ensemble ? ")
-    print("Que répondez-vous ?")
     L_réponse1 = ["Bien sûr, assieds-toi !", "Désolé, je préfère voyager seul."]
-    réponse1 = demander_choix("", L_réponse1)
+    réponse1 = demander_choix("Que répondez-vous ?", L_réponse1)
     if réponse1 == 1 :
         joueur["Attributs"]["loyauté"] += 1
         print("Ron sourit : — Génial ! Tu verras, Poudlard, c’est incroyable ! ")
@@ -18,9 +17,8 @@ def rencontrer_amis(joueur) :
     
     print("Une fille entre ensuite, portant déjà une pile de livres.")
     print("— Bonjour, je m’appelle Hermione Granger. Vous avez déjà lu ‘Histoire de la Magie’ ?")
-    print("Que répondez-vous ?")
     L_réponse2 = ["Oui, j’adore apprendre de nouvelles choses !", "Euh… non, je préfère les aventures aux bouquins."]
-    réponse2 = demander_choix("", L_réponse2)
+    réponse2 = demander_choix("Que répondez-vous ?", L_réponse2)
     if réponse2 == 1 :
         joueur["Attributs"]["intelligence"] += 1
         print("Hermione : — Super! Dans Histoire de la Magie, quel événement majeur Bathilda Tourdesac identifie-t-elle comme la cause première de l’adoption du Code International du Secret Magique ?")
@@ -31,9 +29,8 @@ def rencontrer_amis(joueur) :
     
     print("Puis un garçon blond entre avec un air arrogant.")
     print("— Je suis Drago Malefoy. Mieux vaut bien choisir ses amis dès le départ, tu ne crois pas ?")
-    print("Comment réagissez-vous ? ")
     L_réponse3 = ["Je lui serre la main poliment.", "Je l’ignore complètement.", "Je lui réponds avec arrogance."]
-    réponse3 = demander_choix("", L_réponse3)
+    réponse3 = demander_choix("Comment réagissez-vous ?", L_réponse3)
     if réponse3 == 1 :
         joueur["Attributs"]["ambition"] += 1
         print("Drago : — HaHaHa! ")
@@ -99,6 +96,7 @@ j1 = {
 
 
 lancer_chapitre_2(j1)
+
 
 
 
