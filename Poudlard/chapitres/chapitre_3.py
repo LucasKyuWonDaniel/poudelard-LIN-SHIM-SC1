49089327aca04983fcd678022c5e330bed029010
LucasKyuWonDaniel <kyu.shim@efrei.net>
2025-12-17 17:33:37 +0100

    Update chapitre_3.py

--- Poudlard/chapitres/chapitre_3.py
+++ Poudlard/chapitres/chapitre_3.py
@@ -37,17 +37,43 @@ def apprendre_sorts(joueur, chemin_fichier="../data/sorts.json"):
     print("Voici les sortilèges que tu maîtrises désormais :")
     for i in range(5) :
         for i in range(24) :
-                 if sorts[i]["nom"] == joueur["Sortilèges"][i] :
-                     type = sorts[i]["type"]
+            if sorts[i]["nom"] == joueur["Sortilèges"][i] :
+                type = sorts[i]["type"]
         for i in range(24) :
-                 if sorts[i]["nom"] == joueur["Sortilèges"][i] :
-                     description = sorts[i]["description"]
+           if sorts[i]["nom"] == joueur["Sortilèges"][i] :
+                description = sorts[i]["description"]
         print("- {} ({}) : {}".format(joueur["Sortilèges"][i], type, description))
 
 
 def quiz_magie(joueur, chemin_fichier="../data/quiz_magie.json") :
     print("Bienvenue au quiz de magie de Poudlard !")
     print("Réponds correctement aux 4 questions pour faire gagner des points à ta maison.")
+    L_quiz = load_fichier(chemin_fichier)
+    quiz_pose = []
+    score = 0
+    for i in range(1, 5) :
+        quiz = choice(L_quiz)
+        while quiz in quiz_pose :
+                quiz = choice(L_quiz)
+        reponse = demander_phrase("{}. {}".format(i, quiz["question"]))
+        print(">", reponse)
+        if reponse == quiz["reponse"] :
+                print("Bonne réponse ! +25 points pour ta maison.")
+                score += 25
+        else :
+                print("Mauvaise réponse. La bonne réponse était :", quiz["reponse"])
+        quiz_pose.append(quiz)
+    print("Score obtenu :", score, "points")
+    return score
+        
+def lancer_chapitre_3(personnage, maisons) :
+        apprendre_sorts(joueur, chemin_fichier="../data/sorts.json")
+        score = quiz_magie(joueur, chemin_fichier="../data/quiz_magie.json")
+        actualiser_points_maison(points_maisons,joueur["Maison"], score)
+        afficher_maison_gagnante(points_maisons)
+        afficher_joueur(joueur)
+
+
 
 j1 = {
     "Nom" : "SHIM" ,
