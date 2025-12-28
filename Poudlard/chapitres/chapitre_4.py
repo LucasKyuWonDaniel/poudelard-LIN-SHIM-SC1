a49c6d2b2d195fbcad2a795b7d53c2f7868e1367
LucasKyuWonDaniel <kyu.shim@efrei.net>
2025-12-28 14:34:27 +0100

    Update chapitre_4.py

---
+++ Poudlard/chapitres/chapitre_4.py
@@ -0,0 +1,8 @@
+def creer_equipe(maison, equipe_data, est_joueur=False, joueur=None) :
+    equipe = { 
+        ['nom']: maison, 
+        ['score']: 0, 
+        ['a_marque']: 0, 
+        ['a_stoppe']: 0, 
+        ['attrape_vifdor']: False, 
+        ['joueurs']:
